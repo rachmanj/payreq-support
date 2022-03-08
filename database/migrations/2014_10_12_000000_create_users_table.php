b7d6929b8b434a6199efd73d41caaffab111319e
rachmanj <rachmanj@gmail.com>
2022-03-08 10:37:50 +0800

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
+            $table->unsignedBigInteger('employee_id')->nullable();
             $table->string('email')->unique();
+            $table->string('project_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('is_active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
